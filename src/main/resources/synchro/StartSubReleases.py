de0c3b6c529f8258b126b179adde5c45824324b7
Benoit Moussaud <bmoussaud@xebialabs.com>
2017-05-29 11:18:55 +0200

    new bulk task to generate xlr.createReleases task

---
+++ src/main/resources/synchro/StartSubReleases.py
@@ -0,0 +1,44 @@
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+#
+
+
+def new_variable_string(name, value):
+    print "* new variable {0}->{1}".format(name, value)
+    type = Type.valueOf('xlrelease.StringVariable')
+    var = type.descriptor.newInstance(name)
+    var.setValue(value)
+    print "* var %s" % var
+    print "* var %s" % type
+    return var
+
+
+def resolve_template(template):
+    print "* resolve %s" % template
+    search = templateApi.getTemplates(template)
+    if len(search) == 0:
+        raise Exception('Cannot resolve template {0}'.format(template))
+    print search[0]
+    return search[0].id
+
+
+def add_task(title, template):
+    print "* Create a new task %s -> %s " % (title, template)
+    phase = getCurrentPhase()
+    current_task = getCurrentTask()
+
+    task = taskApi.newTask("xlrelease.CreateReleaseTask")
+    task.title = "Start {0}".format(title)
+    task.newReleaseTitle = title
+    task.templateId = resolve_template(template)
+    task.startRelease = True
+    task.releaseTags = set([title])
+    task.setTemplateVariables([new_variable_string('${LR}', getCurrentRelease().title)])
+    taskApi.addTask(phase.id, task)
+
+
+for key, value in subTasks.iteritems():
+    print "-------------------------"
+    add_task(key, value)
--- src/main/resources/synchro/UpdateGate.py
+++ src/main/resources/synchro/UpdateGate.py
@@ -4,24 +4,48 @@
 # FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
 #
 
-import sys
-
-from com.xebialabs.deployit.plugin.api.reflect import Type
 from com.xebialabs.xlrelease.api.v1.forms import Condition
 
-def get_gate_task(task_name):
-     return taskApi.searchTasksByTitle(task_name,None,release.id)[0]
 
-def new_dependency(release_name,phase_name, task_name):
-    print "* Dependency is %s/%s/%s" % (release_name,phase_name, task_name)
+def add_gate_task(title, env):
+    print "* Create a new  gate with {0} title".format(title)
+    phase = getCurrentPhase()
+    current_task = getCurrentTask()
+    task = taskApi.newTask("xlrelease.GateTask")
+    task.title = title
+    task.pythonScript.xldeployServer = current_task.getPythonScript().getProperty("xldeployServer")
+    task.pythonScript.deploymentPackage = version
+    task.pythonScript.environment = env
+    task.pythonScript.displayStepLogs = False
+    taskApi.addTask(phase.id, task)
+
+
+def get_gate_task(task_name):
+    search = taskApi.searchTasksByTitle(task_name, None, release.id)
+    if len(search) == 0:
+        raise Exception("* ERROR: No tasks with '{0}' title in this release".format(task_name))
+    return search[0]
+
+
+def new_dependency(release_name, phase_name, task_name):
+    print "* Dependency is %s/%s/%s" % (release_name, phase_name, task_name)
+    if len(releaseApi.searchReleasesByTitle(release_name)) == 0:
+        raise Exception("* ERROR: the'{0}' release not found".format(release_name))
+
     master_release = releaseApi.searchReleasesByTitle(release_name)[0]
+
     result = taskApi.searchTasksByTitle(task_name, phase_name, master_release.id)
+    if len(result) == 0:
+        raise Exception(
+            "* ERROR: No tasks with '{0}' title in the '{1}' phase in the '{2}' release".format(task_name, phase_name,
+                                                                                                release_name))
     target = result[0]
     return target
 
+
 task = get_gate_task(gateTaskName)
 print "* Gate task is %s" % task.title
-target = new_dependency(masterReleaseName,masterPhaseName, masterTaskName)
+target = new_dependency(masterReleaseName, masterPhaseName, masterTaskName)
 taskApi.addDependency(task.id, target.id)
 
 if addAConditionInTheMasterRelease:
@@ -31,10 +55,15 @@ if addAConditionInTheMasterRelease:
         print "* '%s' is already in the conditions" % condition.title
     else:
         print "* add the '%s' in the condition condition" % condition.title
-        taskApi.addCondition(target.id,condition)
+        taskApi.addCondition(target.id, condition)
 
+generateGateCheckTask = False
+groupName = 'Check All the deployments are ok'
+if generateGateCheckTask:
+    print '* Generate Gate Check Task {0}/{1}/{2}'.format(masterReleaseName, masterPhaseName, groupName)
+    group_id = new_dependency(masterReleaseName, masterPhaseName, groupName)
+    print group_id
+    raise Exception('X')
+    search = taskApi.searchTasksByTitle(groupName, None, release.id)
 
-
-
-
-
+print "* Done"
